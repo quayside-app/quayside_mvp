458de4722d8bdf9e799c3ffd20fff536a1227882
Patrick Pragman <pat@pragman.io>
2023-09-06 20:16:45 -0800

    refactored for linter

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
 from chatGPT import query_chat_gpt
 
 app = Flask(__name__)
 
 @app.route('/')
-def main_page():  # put application's code here
+def main_page():
     return render_template("main.html")
 
-#take subprompt
-#make sure it is a post request
-#send to ChatGPT
-@app.route('/process', methods = ['POST'])
+@app.route('/process', methods=['POST'])
 def process():
-
     if request.method == 'POST':
-        """modify/update the information for <user_id>"""
-        # you can use <user_id>, which is a str but could
-        # changed to be int or whatever you want, along
-        # with your lxml knowledge to make the required
-        # changes
-        data = request.form # a multidict containing POST
+        data = request.form
         subprompt = data['prompt']
         return render_template("response.html", chat_gpt_response=query_chat_gpt(subprompt))
     else:
-        return "only post supported"
-
+        return "Only POST requests are supported"
 
 if __name__ == '__main__':
     app.run()
