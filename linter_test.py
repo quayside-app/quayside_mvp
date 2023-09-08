5f705e2f1246b53a3a320d15394e527c3f735ca7
Patrick Pragman <pat@pragman.io>
2023-09-08 15:17:14 -0800

    working with gh actions

--- linter_test.py
+++ linter_test.py
@@ -3,8 +3,14 @@ def linter_does_it_work(test: str) -> int:
     """
     This function returns the number 1.
 
+    Testing start of pipeline again
+
     :param test: A dummy variable.
     :return: The number 1.
     """
     print(test)
     return 1
+
+
+if __name__ == "__main__":
+    print(linter_does_it_work("test!"))
