fbea2a297545629e2585cd90fb31b7016a7b42fd
Mya Schroder <80082701+schromya@users.noreply.github.com>
2023-09-19 10:07:16 -0800

    Fixed popup modal issue

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -13,18 +13,18 @@ import tableIcon from "../../public/svg/table.svg";
 import teamIcon from "../../public/svg/team.svg";
 import targetIcon from "../../public/svg/target.svg";
 
-//export default function LeftSidebar() => {
 export default function LeftSidebar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
         <div className="flex w-1/6 bg-gray-800 text-white justify-center rounded-md py-5 ">
-            <NewProjectModal handleClose={() => setIsOpen(true)} isOpen={isOpen}/>
+
+            <NewProjectModal handleClose={() => setIsOpen(false)} isOpen={isOpen}/>
+
             <div className="flex flex-wrap">
               <ul className="font-medium">
                 <li> <span className="ml-3 flex justify-center py-5">Directory</span> </li>
-                
 
-                <li> <Dropdown label = "New Project" clickAction={() => {setIsOpen(true); console.log("Here")} } image_path={plusIcon}/> </li>
+                <li> <Dropdown clickAction ={() => {setIsOpen(true); console.log("Here")}} label = "New Project"  image_path={plusIcon}/> </li>
                 <li> <Dropdown label = "Task" image_path={plusIcon}/> </li>
 
                 <li><div className="pt-4 my-10 space-y-2 font-medium border-t  border-gray-200"></div></li>
--- src/components/NewProjectModal.js
+++ src/components/NewProjectModal.js
@@ -7,33 +7,31 @@ import xIcon from "../../public/svg/x.svg";
 import Image from "next/image";
 
 
-const NewProjectModal = ({isOpen, handleClose }) => {
-    console.log("Yeehaw", isOpen)
-
+const NewProjectModal = ({isOpen, handleClose}) => {
     if (!isOpen) return null;
     return (
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 ">
-            <div id="authentication-modal" tabIndex="-1"  className="fixed w-full p-4 ">
-            
-                    <div className="relative rounded-lg shadow bg-gray-900">
-                        <button type="button" onClick={handleClose} className="absolute top-3 right-3 rounded-lg  w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600" data-modal-hide="authentication-modal">
-                            <Image src={xIcon} alt="exit" width="10" height ="10"/>
-                        </button>
-                        <div className="px-6 py-6 lg:px-8">
-                            <h3 className="mb-4 text-xl font-medium text-white">New Project</h3>
-                            <form className="space-y-6" action="#">
-                                <Input label="ChatGPT API Key" placeholder="••••••••"/>
-                                <Input label="Project Description" placeholder="I want to bake a cake!"/>
-                        
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75">
+                <div id="authentication-modal" tabIndex="-1"  className="fixed w-full p-4 ">
+                
+                        <div className="relative rounded-lg shadow bg-gray-900">
+                            <button type="button" onClick={handleClose} className="absolute top-3 right-3 rounded-lg  w-8 h-8 inline-flex justify-center items-center hover:bg-gray-600">
+                                <Image src={xIcon} alt="exit" width="10" height ="10"/>
+                            </button>
+                            <div className="px-6 py-6 lg:px-8">
+                                <h3 className="mb-4 text-xl font-medium text-white">New Project</h3>
+                                <form className="space-y-6" action="#">
+                                    <Input label="ChatGPT API Key" placeholder="••••••••"/>
+                                    <Input label="Project Description" placeholder="I want to bake a cake!"/>
 
-                                <button className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
-
-                            </form>
+                                    <button className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                        Create
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
+            </div>
 
-        </div>
         
     );
 };
