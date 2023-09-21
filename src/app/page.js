5090f3ebbadb864c7ce2ebfeaee30ad788f49290
Ashtonrcurry <ashtonrcurry@hotmail.com>
2023-09-20 17:15:58 -0800

    fixed some Dockerfile settings and modified the next.config.js file to work with docker

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p>It's our MVP!</p>
+        <p>Its our MVP!</p>
 
         <button
           onClick={async () => {
