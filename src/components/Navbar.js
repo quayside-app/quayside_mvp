f613cae30cde3318c8f475ca39d380c97cade9f2
Mya Schroder <80082701+schromya@users.noreply.github.com>
2023-09-21 14:18:06 -0800

    Fixed navbar spacing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,34 +7,41 @@ const Navbar = () => {
     return (
 
       <div className="px-5">
-        <nav className="rounded-md bg-neutral-800 p-4 text-white">
+        <nav className="rounded-md bg-neutral-800 p-2 text-white w-full">
 
           <div className="flex flex-wrap"> {/* Stack elements on mobile, grid on larger screens */}
             
-            {/* Hamburger */}
-            <div className="flex w-1/12 items-center "> {/* Added bottom margin for mobile */}
-                <button className="text-white text-3xl md:text-4xl font-bold opacity-70 hover:opacity-100"> &#9776; </button>
-            </div>
+            <div className="flex w-2/12 lg:w-1/12 justify-start mx-1">
+              {/* Hamburger */}
+              <div className="flex">
+                  <div className="">
+                    <button className="text-white text-3xl md:text-4xl font-bold opacity-70 hover:opacity-100 align-super"> &#9776; </button>
+                  </div>
+              </div>
 
-            {/* Logo */}
-            <div className="w-1/12 mx-2">
-              <Image src={logo} height={30} width={30} className="" /> {/* Smaller logo on mobile */}
+              {/* Logo */}
+              <div className="flex mx-2">
+                <div className="my-auto">
+                  <Image src={logo} height={30} width={30} className="" /> 
+                </div>
+              </div>
             </div>
   
             {/* Current Directory */}
-            <div className="flex w-4/12"> {/* Centered on mobile, left on larger screens */}
-    
-              <input type="text" className=" bg-neutral-600 rounded-3xl px-4 text-xs "
-                defaultValue="/quayside/Your-App"/>
+            <div className="flex w-4/12 justify-star mx-1"> 
+              <div className="flex bg-neutral-600 px-4 rounded-3xl overflow-hidden">
+                <input type="text" minWidth="0" className="bg-neutral-600  text-xs"
+                  defaultValue="/quayside/Your-App"/>
+              </div>
             </div>
   
             {/* Search Bar */}
-            <div className="flex w-4/12"> {/* Centered on mobile, right on larger screens */}
+            <div className="flex w-5/12 justify-end mx-1">
               <div className="flex overflow-hidden  bg-neutral-600 rounded-3xl">
                 <input
                   type="search"
                   placeholder="Search..."
-                  className="flex py-1 px-3 text-xs  bg-neutral-600 text-white md:text-base"
+                  className="flex  px-3 text-xs  bg-neutral-600 text-white md:text-base"
                 />
                 <button className="px-4  flex items-center justify-center">
                   <NavbarSvg height="12px" width="12" color="#FFFFFF" className="md:h-15 md:w-15" /> {/* Smaller icon on mobile */}
