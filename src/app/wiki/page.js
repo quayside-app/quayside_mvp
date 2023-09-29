f637074d3a397d4b6b05d1bc1eb6382c472afc9e
Mya Schroder <80082701+schromya@users.noreply.github.com>
2023-09-29 11:16:49 -0800

    Added quayside.app/wiki to route to wiki

---
+++ src/app/wiki/page.js
@@ -0,0 +1,9 @@
+"use client";
+import {useEffect} from 'react'
+
+export default function redirectToWiki() {
+    useEffect(() => {
+        window.location.assign('https://quayside-app.github.io/Wiki/#/')
+    })
+    return(<></>)
+}
