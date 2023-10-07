8e3056ceb5543fa0f65d51c2e2247592e8ec89f8
Mya Schroder <80082701+schromya@users.noreply.github.com>
2023-10-07 15:59:15 -0800

    Took out extra stuff

--- src/app/page.js
+++ src/app/page.js
@@ -12,9 +12,9 @@ export default function Home () {
   return (
     <main className='flex flex-wrap w-full flex-col items-center'>
       <div className='flex w-full flex-wrap items-center'>
-        
-        <div className='w-full'><TreeGraph /></div>
         <DataDisplay/>
+        <div className='w-full'><TreeGraph /></div>
+        
       </div>
       
     </main>
--- src/components/User.js
+++ src/components/User.js
@@ -2,16 +2,14 @@ import mongoose from 'mongoose';
 import {User} from '../api/mongoModels';
 
 async function getData() {
-    //const { db } = await connectToDatabase();
     const username = process.env.MONGO_USERNAME;
     const password = process.env.MONGO_PASSWORD;
 
     const uri = `mongodb+srv://${username}:${password}@quayside-cluster.ry3otj1.mongodb.net/quayside?retryWrites=true&w=majority`;
     try {
       await mongoose.connect(uri);
-      const data = await User.findOne({firstName: 'Fred'}).exec();
+      const data = await User.findOne({firstName: 'Fred'});
       await mongoose.disconnect();
-      console.log(data)
       return data;
   } catch (error) {
       console.error('Error fetching data:', error);
@@ -19,14 +17,13 @@ async function getData() {
   }
 }
 
-
 export default async function DataDisplay() {
     const data = await getData()
    
     return (
         <div>
           <h1>Dis User:</h1>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
+          <div>{JSON.stringify(data, null, 2)}</div>
         </div>
       );
 }
