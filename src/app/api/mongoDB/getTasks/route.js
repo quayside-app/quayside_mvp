16fdc25a070123aa19e22db976714e163a96ac6f
Mya Schroder <80082701+schromya@users.noreply.github.com>
2023-10-18 19:15:46 -0800

    Added comment

--- src/app/api/mongoDB/getTasks/route.js
+++ src/app/api/mongoDB/getTasks/route.js
@@ -3,7 +3,30 @@ import { NextResponse } from "next/server";
 import {Task} from '../mongoModels';
 import {URI} from '../mongoData.js';
 
-
+/**
+ * An asynchronous function to handle GET requests to retrieve tasks based on either a project ID or a task ID.
+ * If neither projectID nor taskID is provided, a 400 Bad Request response is returned.
+ * In case of a database or other error, a 500 Internal Server Error response is returned.
+ *
+ * @async
+ * @function
+ * @param {Object} NextRequest[request] - The HTTP request object.
+ * @returns {Object} - Returns a list of JSON tasks if successful, or an JSON object with an error message if unsuccessful. The HTTP status code is also returned.
+ *
+ * @example
+ * // Example usage:
+ *  fetch(`/api/mongoDB/getTasks?projectID=1234`, {
+ *     method: 'GET', 
+ *     headers: { 'Content-Type': 'application/json' },
+ *  }).then(async (response) => {
+ *     let body = await response.json();
+ *     if (!response.ok) {
+ *         console.error(body.message);
+ *     } else {
+ *         console.log(body);
+ *     }
+ * }).catch(error => console.error(error));
+ */
 export async function GET(request) {
     try {
         // const { projectID, taskID } = await request.query;
@@ -31,20 +54,3 @@ export async function GET(request) {
         return NextResponse.json({ message: 'Error getting data ' + error }, { status: 500 });
     }
 }
-
-
-        // // Require projectID or taskID, 
-        // if (!params.projectID && !params.taskID) {
-        //     return NextResponse.json({ message: "Project ID or Task ID required." }, { status: 400 });
-        //     }
-    
-        //     if (mongoose.connection.readyState !== 1) await mongoose.connect(URI);
-    
-        //     let tasks = "";
-        //     if (params.taskID) {
-        //         tasks = await Task.find({ _id: params.taskID});
-        //     }else if (params.projectID) {
-        //         tasks = await Task.find({ projectID: params.projectID});
-        //     }
-        //     console.log(tasks);
-        //     return NextResponse.json({tasks}, {status:200});
