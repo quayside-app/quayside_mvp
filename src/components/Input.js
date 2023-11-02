617967ecd2e0f1e2990411e2e231846c44d62553
erwilliams64 <37854636+erwilliams64@users.noreply.github.com>
2023-11-01 19:13:02 -0800

    Update Input.js to remove the "required" parameter

--- src/components/Input.js
+++ src/components/Input.js
@@ -23,7 +23,7 @@ const Input = ({ name, value, changeAction, label, placeholder }) => {
 
     <div>
       <label className='block mb-2 text-sm font-medium text-white'>{label} </label>
-      <input name={name} value={value} onChange={changeAction} placeholder={placeholder} type='text' className=' text-black border  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dbg-gray-600 border-gray-500 placeholder-gray-400' required />
+      <input name={name} value={value} onChange={changeAction} placeholder={placeholder} type='text' className=' text-black border  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dbg-gray-600 border-gray-500 placeholder-gray-400' />
     </div>
 
   )
