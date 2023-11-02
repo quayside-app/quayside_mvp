4fc3e459013736c7c7e4f029c4f5b6ce63252e4e
Ashtonrcurry <ashtonrcurry@hotmail.com>
2023-11-01 20:28:47 -0800

    working for sure

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 //protects entire site instead of select pages
 export { default } from "next-auth/middleware"
-
 /**
  * Applies authentication to select pages
  * docs: https://nextjs.org/docs/pages/building-your-application/routing/api-routes
